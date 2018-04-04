aa1db228cbe08f51fc3d8a3c58f600405216c9a9
Lee Steiner <leealansteiner@gmail.com>
2018-04-04 14:27:05 -0400

    initial

--- FunWithStructures/Program.cs
+++ FunWithStructures/Program.cs
@@ -10,6 +10,55 @@ namespace FunWithStructures
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** A First Look at Structures *****\n");
+
+            //Create an initial point.
+            Point myPoint;
+            myPoint.x = 349;
+            myPoint.y = 76;
+            myPoint.Display();
+
+            //Adjust the x and y values.
+            myPoint.Increment();
+            myPoint.Display();
+            Console.ReadLine();
+
+            Point p2 = new Point(50, 60);
+            p2.Display();
+        }
+
+
+        struct Point
+        {
+            //Fields of the structure
+            public int x;
+            public int y;
+
+            //Add 1 to the (x,y) position
+            public void Increment()
+            {
+                x++;
+                y++;
+            }
+
+            //Subtract 1 from the (x,y) position
+            public void Decrement()
+            {
+                x--;
+                y--;
+            }
+
+            //Display the current position.
+            public void Display()
+            {
+                Console.WriteLine($"X = {x}, y= {y}");
+            }       
+
+            public Point(int XPos, int YPos)
+            {
+                x = XPos;
+                y = YPos;
+            }
         }
     }
 }
